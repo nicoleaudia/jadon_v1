f7ec7846a07df6e10ee8a6b6939e998ccb3f7849
nicoleaudia <nma23@imperial.ac.uk>
2024-01-27 18:36:23 +0000

    successfully uses openai API to print test message using key in env

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+openai_api_key = os.environ.get('OPENAI_API_KEY')
+# client = OpenAI(api_key=openai_api_key)
+
+#from teacher.py import (fxn name)
+
 
 # Create a Flask app instance
 app = Flask(__name__)
 
 # Define a route and a view function
-@app.route('/')
+@app.route('/') #REMEMBER THIS IS TEST
 def hello_world():
-    return 'Hello, World!'
+    #client = OpenAI(api_key='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')
+    client = OpenAI(api_key=openai_api_key)
+
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[{"role": "user", "content": "Say this is a test"}],
+        stream=True,
+    )
+    s = ""
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            s += chunk.choices[0].delta.content
+    return s
+
+
+
+@app.route('/testtest')
+def index():
+    return render_template('index.html')
+
+@app.route('/teacher')
+def teacher():
+    return render_template('teacher.html')
+
 
 # Run the application
 if __name__ == '__main__':
