77b16eb94dc2dc80a8be13dc5da1415a48796fe1
nicoleaudia <nma23@imperial.ac.uk>
2024-01-27 21:16:11 +0000

    updated env and venv related to key

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ app = Flask(__name__)
 # Define a route and a view function
 @app.route('/testtest')
 def hello_world():
-    #client = OpenAI(api_key='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')
     client = OpenAI(api_key=openai_api_key)
 
     stream = client.chat.completions.create(
